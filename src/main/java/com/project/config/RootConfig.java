fdef5d2b1041ebf2bc12914d2538ceac0a2be032
kimisak <isak7263@gmail.com>
2021-05-15 00:55:23 +0900

    mybatis dependency 추가 및 java로 sqlSesseionFactory bean 세팅

--- src/main/java/com/project/config/RootConfig.java
+++ src/main/java/com/project/config/RootConfig.java
@@ -2,6 +2,8 @@ package com.project.config;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -22,5 +24,12 @@ public class RootConfig {
 		hikariConfig.setPassword("isaackiss");
 		HikariDataSource dataSource =  new HikariDataSource(hikariConfig);
 		return dataSource;
+	}
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception {
+		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		sqlSessionFactoryBean.setDataSource(dataSource());
+		return (SqlSessionFactory) sqlSessionFactoryBean.getObject();
 	}
 }
